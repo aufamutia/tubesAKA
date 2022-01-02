1f50bce6ecc8472935434859d85fb0073bc211d0
zefrinaldi <90541401+zefrinaldi@users.noreply.github.com>
2022-01-02 07:28:08 +0700

    Update tubesAKA.cpp

--- tubesAKA.cpp
+++ tubesAKA.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
-
 using namespace std;
 
 int main(){
-  cout << "hello world" << endl;
-  return 0;
+    cout<<"======Insertion Sort======"<<endl<<endl;
+	int y;
+	cout<<"Masukan banyak array: ";
+	cin>>y;
+	cout<<endl;
+	int x[y];
+	for(int i=0; i<y; i++){
+		cout<<"Masukan angka ke "<<i<<": ";
+		cin>>x[i];
+	}
+	cout<<endl;
+	cout<<"Proses sorting"<<endl;
+	for(int i=1; i<y; i++){
+		int key = x[i];
+		int j = i-1;
+		while(j>=0 && x[j] > key){
+			x[j+1] = x[j];
+			j--;
+		}
+		x[j+1] = key;
+		for(int m=0;m<y;m++){
+		cout<<x[m]<<" ";
+	    }
+	    cout<<endl;
+	}
+	cout<<endl;
+	cout<<"Hasil akhir"<<endl;
+	for(int m=0;m<y;m++){
+		cout<<x[m]<<" ";
+	}
+	cout<<endl;
 }
-
